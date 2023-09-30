9a3dfa814acbc0a32d326ee5daa8ffbcc327a687
akdon1198 <akdon1198@gmail.com>
2023-09-30 15:29:43 +0530

    boilerplate react setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return(
+    <>
+    <div>
+
+      <div style={{width:"100vw", height:"100vh", background:"pink"}}>      </div>
+      <div></div>
+    </div>
+    </>
+  )
+}
+
+export default App;
